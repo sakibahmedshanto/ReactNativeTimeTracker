c08f955f97c3053ddcbe78510b60479be120df4b
sakibahmedshanto <sakibsanto57@gmail.com>
2025-07-25 03:11:51 +0600

    creating a new timer is working fine

---
+++ App.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { StyleSheet, View, ScrollView, Text } from 'react-native';
+import EditableTimer from './components/EditableTimer';
+import ToggleableTimerForm from './components/ToggleableTimerForm';
+import { newTimer } from './Utils/TimerUtils';
+import * as Crypto from 'expo-crypto';
+
+
+export default class App extends React.Component {
+  state = {
+    timers: [
+      {
+        title: 'Mow the lawn',
+        project: 'House Chores',
+        id: Crypto.randomUUID(),
+        elapsed: 5456099,
+        isRunning: true,
+      },
+      {
+        title: 'Bake squash',
+        project: 'Kitchen Chores',
+        id: Crypto.randomUUID(),
+        elapsed: 1273998,
+        isRunning: false,
+      },
+    ],
+  };
+  handleCreateFormSubmit = (timer) => {
+    const { timers } = this.state;
+    this.setState({
+      timers: [newTimer(timer), ...timers],
+    });
+  };
+  render() {
+    const { timers } = this.state;
+    return (
+      <View style={styles.appContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Timers</Text>
+        </View>
+        <ScrollView style={styles.timerList}>
+          <ToggleableTimerForm onFormSubmit={this.handleCreateFormSubmit} />
+          {timers.map(({ title, project, id, elapsed, isRunning }) => (
+            <EditableTimer
+              key={id}
+              id={id}
+              title={title}
+              project={project}
+              elapsed={elapsed}
+              isRunning={isRunning}
+            />
+          ))}
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+  },
+  titleContainer: {
+    paddingTop: 35,
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#D6D7DA',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  timerList: {
+    paddingBottom: 15,
+  },
+});
